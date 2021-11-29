d5b66f150887bf2d8926ffc287be5dfbd7ec63a9
Anderson Choren <andersonchoren@gmail.com>
2021-11-29 20:46:01 -0300

    Projeto Your New Car Concluído

--- yournewcar/src/main/java/br/com/andersonchoren/yournewcar/controller/CarController.java
+++ yournewcar/src/main/java/br/com/andersonchoren/yournewcar/controller/CarController.java
@@ -1,9 +1,15 @@
 package br.com.andersonchoren.yournewcar.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,5 +34,37 @@ public class CarController {
             return new ResponseEntity<>(new CarDTO(), HttpStatus.NOT_FOUND);
         }
         return new ResponseEntity<>(new CarDTO(), HttpStatus.BAD_REQUEST);
+    }
+
+    @PutMapping
+    public ResponseEntity<CarDTO> update(@RequestBody CarDTO carDTO) {
+        if (carDTO != null) {
+            var result = carService.insertOrUpdate(carDTO);
+
+            if (result != null) {
+                return new ResponseEntity<>(result, HttpStatus.CREATED);
+            }
+            return new ResponseEntity<>(new CarDTO(), HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(new CarDTO(), HttpStatus.BAD_REQUEST);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CarDTO>> findAll() {
+        var listOfCars = carService.findAll();
+        return new ResponseEntity<>(listOfCars, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public CarDTO findById(@PathVariable int id) {
+        return carService.findById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@PathVariable int id) {
+        var isSuccess = carService.delete(id);
+        if (isSuccess)
+            return new ResponseEntity<>("Carro removido com sucesso!!!", HttpStatus.OK);
+        return new ResponseEntity<>("Não foi possível remover o carro informado!!!", HttpStatus.NOT_FOUND);
     }
 }
--- yournewcar/src/main/java/br/com/andersonchoren/yournewcar/service/CarService.java
+++ yournewcar/src/main/java/br/com/andersonchoren/yournewcar/service/CarService.java
@@ -1,13 +1,11 @@
 package br.com.andersonchoren.yournewcar.service;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import br.com.andersonchoren.yournewcar.model.Car;
 import br.com.andersonchoren.yournewcar.model.dto.CarDTO;
 import br.com.andersonchoren.yournewcar.repository.CarRepository;
 import br.com.andersonchoren.yournewcar.util.CarMapping;
@@ -21,10 +19,6 @@ public class CarService {
         var result = carRepository.save(CarMapping.toCar(car));
         return CarMapping.fromCar(result);
     }
-
-    // public Car insertOrUpdate(Car car) {
-    // return carRepository.save(car);
-    // }
 
     public List<CarDTO> findAll() {
         return carRepository.findAll().stream().map(car -> CarMapping.fromCar(car)).collect(Collectors.toList());
@@ -40,10 +34,14 @@ public class CarService {
     }
 
     public boolean delete(int id) {
-        carRepository.deleteById(id);
-
         var result = carRepository.findById(id);
-        return result.isEmpty();
+        if(result.isPresent()){
+            carRepository.deleteById(id);
+            result = carRepository.findById(id);
+            return result.isEmpty();
+        }
+        else
+            return false;
     }
 
 }
