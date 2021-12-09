947dae2dba191d1e09735cdc8788437aa4f140cf
Anderson Choren <andersonchoren@gmail.com>
2021-12-09 20:14:35 -0300

    Projeto Library com Thymeleaf

---
+++ library/src/main/java/br/com/andersonchoren/library/controller/APIBookController.java
@@ -0,0 +1,86 @@
+package br.com.andersonchoren.library.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.andersonchoren.library.model.dto.BookDTO;
+import br.com.andersonchoren.library.service.BookService;
+import br.com.andersonchoren.library.util.BookMapping;
+
+@RestController
+@RequestMapping("/api/books")
+public class APIBookController {
+    @Autowired
+    private BookService service;
+
+    @PostMapping
+    public ResponseEntity<BookDTO> insert(@RequestBody BookDTO dto) {
+        if (dto.getId() != 0)
+            return new ResponseEntity<>(new BookDTO(), HttpStatus.BAD_REQUEST);
+
+        var book = service.insertOrUpdate(
+                BookMapping.toBook(dto));
+
+        if (book != null)
+            return new ResponseEntity<>(
+                    BookMapping.fromBook(book), HttpStatus.CREATED);
+        return new ResponseEntity<>(new BookDTO(), HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping
+    public ResponseEntity<BookDTO> update(@RequestBody BookDTO dto) {
+        if (dto.getId() < 1)
+            return new ResponseEntity<>(new BookDTO(), HttpStatus.BAD_REQUEST);
+
+        var book = service.findById(dto.getId());
+
+        if (book.isPresent())
+            return new ResponseEntity<>(
+                    BookMapping.fromBook(
+                            service.insertOrUpdate(
+                                    BookMapping.toBook(dto))),
+                    HttpStatus.OK);
+        return new ResponseEntity<>(new BookDTO(), HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<BookDTO>> findAll() {
+        return new ResponseEntity<>(
+                service.findAll().stream().map(
+                        (obj) -> BookMapping.fromBook(obj)).collect(Collectors.toList()),
+                HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(@PathVariable int id) {
+        var book = service.findById(id);
+        if (book.isPresent()) {
+            var isSuccess = service.delete(id);
+
+            if (isSuccess)
+                return new ResponseEntity<>("Livro removido com sucesso!!!", HttpStatus.OK);
+            return new ResponseEntity<>("Não foi possível remover o livro!!!", HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>("Livro não localizado!!!", HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping("/author/{name}")
+    public ResponseEntity<List<BookDTO>> findByAuthor(@PathVariable String name) {
+        return new ResponseEntity<>(
+                service.findByAuthor(name).stream().map(
+                        (obj) -> BookMapping.fromBook(obj)).collect(Collectors.toList()),
+                HttpStatus.OK);
+    }
+}
--- library/src/main/java/br/com/andersonchoren/library/controller/BookController.java
+++ library/src/main/java/br/com/andersonchoren/library/controller/BookController.java
@@ -1,86 +1,38 @@
 package br.com.andersonchoren.library.controller;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import br.com.andersonchoren.library.model.dto.BookDTO;
 import br.com.andersonchoren.library.service.BookService;
 import br.com.andersonchoren.library.util.BookMapping;
 
-@RestController
+@Controller
 @RequestMapping("/books")
 public class BookController {
     @Autowired
-    private BookService service;
-
-    @PostMapping
-    public ResponseEntity<BookDTO> insert(@RequestBody BookDTO dto) {
-        if (dto.getId() != 0)
-            return new ResponseEntity<>(new BookDTO(), HttpStatus.BAD_REQUEST);
-
-        var book = service.insertOrUpdate(
-                BookMapping.toBook(dto));
-
-        if (book != null)
-            return new ResponseEntity<>(
-                    BookMapping.fromBook(book), HttpStatus.CREATED);
-        return new ResponseEntity<>(new BookDTO(), HttpStatus.NOT_FOUND);
-    }
-
-    @PutMapping
-    public ResponseEntity<BookDTO> update(@RequestBody BookDTO dto) {
-        if (dto.getId() < 1)
-            return new ResponseEntity<>(new BookDTO(), HttpStatus.BAD_REQUEST);
-
-        var book = service.findById(dto.getId());
-
-        if (book.isPresent())
-            return new ResponseEntity<>(
-                    BookMapping.fromBook(
-                            service.insertOrUpdate(
-                                    BookMapping.toBook(dto))),
-                    HttpStatus.OK);
-        return new ResponseEntity<>(new BookDTO(), HttpStatus.NOT_FOUND);
-    }
+    private BookService bookService;
 
     @GetMapping
-    public ResponseEntity<List<BookDTO>> findAll() {
-        return new ResponseEntity<>(
-                service.findAll().stream().map(
-                        (obj) -> BookMapping.fromBook(obj)).collect(Collectors.toList()),
-                HttpStatus.OK);
+    public String formInsert(Model model) {
+        BookDTO book = new BookDTO();
+        model.addAttribute("objBook", book);
+        return "pages/register";
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<String> delete(@PathVariable int id) {
-        var book = service.findById(id);
-        if (book.isPresent()) {
-            var isSuccess = service.delete(id);
-
-            if (isSuccess)
-                return new ResponseEntity<>("Livro removido com sucesso!!!", HttpStatus.OK);
-            return new ResponseEntity<>("Não foi possível remover o livro!!!", HttpStatus.NOT_FOUND);
+    @PostMapping
+    public String insert(@ModelAttribute BookDTO bookDTO) {
+        var book = bookService.insertOrUpdate(
+                BookMapping.toBook(bookDTO));
+        if (book.getId() != 0) {
+            return "pages/success";
+        } else {
+            return "pages/error";
         }
-        return new ResponseEntity<>("Livro não localizado!!!", HttpStatus.NOT_FOUND);
-    }
-
-    @GetMapping("/author/{name}")
-    public ResponseEntity<List<BookDTO>> findByAuthor(@PathVariable String name) {
-        return new ResponseEntity<>(
-                service.findByAuthor(name).stream().map(
-                        (obj) -> BookMapping.fromBook(obj)).collect(Collectors.toList()),
-                HttpStatus.OK);
     }
 }
